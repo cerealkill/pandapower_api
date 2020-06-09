8460867a9ce576399869faf2e4b68856f0e9ed1d
J. Paul Depraz <2923413+cerealkill@users.noreply.github.com>
2020-06-09 11:00:03 +0200

    WiP: Additional test

--- api/controllers/api_v1.py
+++ api/controllers/api_v1.py
@@ -1,7 +1,8 @@
 from flask_restful import reqparse, abort, Resource
+from flask_restful_swagger import swagger
+
 from api.controllers.simulation import SimulationController
 
-# parses
 parser = reqparse.RequestParser()
 parser.add_argument('active', type=float, help='Active load must be a float.')
 parser.add_argument('reactive', type=float, help='Active load must be a float.')
@@ -9,6 +10,7 @@ parser.add_argument('reactive', type=float, help='Active load must be a float.')
 # Static variable storing the state in memory
 simulations = {
     '0': {'results': {'load': {'active': 0.1, 'reactive': 0.05}}, 'id': 0},
+    # '1': SimulationIndex('1', 0.4, 0.6)
 }
 
 
@@ -19,15 +21,51 @@ def abort_if_doesnt_exist(sim_id):
 
 class Simulation(Resource):
 
+    @swagger.operation(
+        notes='Gets simulation by id provided in the url.',
+        responseMessages=[
+            {
+                "code": 404,
+                "message": "Simulation with provided ID was not found."
+            }
+        ]
+    )
     def get(self, sim_id):
         abort_if_doesnt_exist(sim_id)
         return simulations[sim_id]
 
+    @swagger.operation(
+        notes='Deletes simulation by id provided in the url.',
+        responseMessages=[
+            {
+                "code": 204,
+                "message": "Simulation successfully Deleted."
+            },
+            {
+                "code": 404,
+                "message": "Simulation with provided ID was not found."
+            }
+        ]
+    )
     def delete(self, sim_id):
         abort_if_doesnt_exist(sim_id)
         del simulations[sim_id]
         return '', 204
 
+    @swagger.operation(
+        notes='Creates or replaces a simulation of specific id, with grid load specified in the active and reactive '
+              'body parameters in the multipart/form-data format. Defaults are 0.1 active and 0.05 reactive grid load.',
+        responseMessages=[
+            {
+                "code": 201,
+                "message": "Created."
+            },
+            {
+                "code": 404,
+                "message": "Simulation with provided ID was not found."
+            }
+        ]
+    )
     def put(self, sim_id):
         args = parser.parse_args()
         active_load, reactive_load = args['active'], args['reactive']
@@ -39,9 +77,27 @@ class Simulation(Resource):
 
 class SimulationList(Resource):
 
+    @swagger.operation(
+        notes='Gets a list of all existing simulations.',
+    )
     def get(self):
         return simulations
 
+    @swagger.operation(
+        notes='Creates a simulation with grid load specified in the active and reactive body parameters in '
+              'the multipart/form-data format. Defaults are 0.1 active and 0.05 reactive grid load.'
+              'Simulation id is in the response body in json format.',
+        responseMessages=[
+            {
+                "code": 400,
+                "message": "Malformed body. Active and Reactive load must be floating points."
+            },
+            {
+                "code": 201,
+                "message": "Created. Simulation ID is in the message body."
+            }
+        ]
+    )
     def post(self):
         args = parser.parse_args()
         active_load, reactive_load = args['active'], args['reactive']
@@ -49,11 +105,21 @@ class SimulationList(Resource):
         results = pp_sim.run_simulation(active_load, reactive_load)
         sim_id = str(int(max(simulations.keys())) + 1) if len(simulations) > 0 else '0'
         simulations[sim_id] = {'results': {'load': {'active': results[0], 'reactive': results[1]}}, 'id': sim_id}
+        # TODO: Add response resource location header
         return simulations[sim_id], 201
 
 
 class Load(Resource):
 
+    @swagger.operation(
+        notes='Gets specific load by simulation id and load type provided in the url.',
+        responseMessages=[
+            {
+                "code": 404,
+                "message": "Simulation with provided ID was not found."
+            }
+        ]
+    )
     def get(self, **kwargs):
         load_type, sim_id = kwargs['load_type'], kwargs['sim_id']
         abort_if_doesnt_exist(sim_id)
--- api/server.py
+++ api/server.py
@@ -7,7 +7,7 @@ from flask_restful_swagger import swagger
 from api.controllers.api_v1 import SimulationList, Simulation, Load
 
 rest = Flask(__name__)
-api_v1 = swagger.docs(Api(rest), apiVersion='1.0')
+api_v1 = swagger.docs(Api(rest), apiVersion='1.0', description='Power grid simulation REST API')
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('flask_cors').level = logging.INFO
@@ -15,7 +15,6 @@ logging.getLogger('flask_cors').level = logging.INFO
 api_v1.add_resource(SimulationList, '/api/v1/simulations')
 api_v1.add_resource(Simulation, '/api/v1/simulation/<sim_id>')
 api_v1.add_resource(Load, '/api/v1/simulation/<sim_id>/load/<load_type>')
-
 
 if __name__ == '__main__':
     rest.run('0.0.0.0', 80)
